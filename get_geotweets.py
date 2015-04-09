4b184b359a65210693aaed19888643ac6026312c
David de Kleer <ddekleer@kpnplanet.nl>
2015-04-09 16:30:48 +0200

    bugfix (code in if-statement had geen indentatie)

--- get_geotweets.py
+++ get_geotweets.py
@@ -5,7 +5,7 @@ def main():
     for line in sys.stdin:
         elements = line.strip().split('\t')
         if elements[1] != '' :
-        tweets.append(elements)
-        print('\t'.join(elements))
+            tweets.append(elements)
+            print('\t'.join(elements))
 
 main()
