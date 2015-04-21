13d87ec9b80331c15abea903fdbf4dbbcb88031d
daviddekleer <ddekleer@kpnplanet.nl>
2015-04-21 12:17:42 +0200

    EventDetective heeft nu echt niets meer met generatie/annotatie van de dataset te maken ;-)

--- EventDetective.py
+++ EventDetective.py
@@ -6,47 +6,14 @@ EventDetective
 ##############
 Detecteert events gegeven dataset
 """
-import os, sys, msgpack, time
-from TweetPreprocessor import TweetPreprocessor
-from ClusterCreator import ClusterCreator
-from ClusterMerger import ClusterMerger
+import os, sys, msgpack, time, json
 
 class EventDetective:
 
-    def __init__(self, tweetFile):
-        # zijn het idf-dictionary en de event candidates al eerder gemaakt?
-        #idf = self.__load_file("idf.bin")
-        #eventCandidates = self.__load_file("eventCandidates.bin")
-        
-        # maak of leeg de map met clusters
+    def __init__(self):
+        self.dataSets = os.listdir('data/')
         self.__emptyClusterFolder()
-        # preprocess tweets in tweetFile
-        preprocessor = TweetPreprocessor(tweetFile)
-        #idf = preprocessor.getIdf()
-        tweetDicts = preprocessor.getTweetDicts()
-        # maak candidate clusters
-        creator = ClusterCreator(tweetDicts)
-        clusters = creator.getClusters()
-        # voeg candidate clusters samen tot event candidates
-        merger = ClusterMerger(clusters)
-        eventCandidates = merger.getEventCandidates()
-        
-        """n = 50
-        print("\n### A selection of events candidates ###\n")
-        count = 0
-        for geohash in eventCandidates:
-            for times in eventCandidates[geohash]:
-                for tweet in eventCandidates[geohash][times]:
-                    print(tweet["user"], tweet["localTime"], tweet["text"])
-            print()
-            count += 1
-            if count == n:
-                break
-        """
-
-        #self.dataSets = os.listdir('data/')
-        #self.loadDataSet()
-        #self.selectEvents()
+        self.__loadDataSet()
         
     # Laad een bestand met msgpack, we moeten nog even bepalen of we dit gaan gebruiken
     def __load_file(self, f):
@@ -57,7 +24,6 @@ class EventDetective:
         except:
             return False    
 
-        
     def __emptyClusterFolder(self):
         if not os.path.isdir("clusters"): 
             # clustermap bestaat nog niet
@@ -70,10 +36,8 @@ class EventDetective:
                 os.remove('clusters/' + f)
             print("Emptied the cluster folder.")
 
-    def loadDataSet(self):
-        pass
-        
-        """for i, dataset in enumerate(self.dataSets):
+    def __loadDataSet(self):
+        for i, dataset in enumerate(self.dataSets):
             print("{}: {}".format(i, dataset))
         choice = int(input("Select dataset: "))
         
@@ -83,7 +47,6 @@ class EventDetective:
 
         jsonFile =  open("data/" + self.dataSets[choice] + "/eventCandidates.json")
         self.candidates = json.load(jsonFile)
-        """
 
     def classifyEvents(self):
         pass
@@ -95,44 +58,8 @@ class EventDetective:
         pass
 
     def selectEvents(self):
-        pass#print(self.candidates['u1kchfe'])   
-        
-    """Dit moet nog even verwerkt worden...
-                
-        # zijn het idf-dictionary en de tweet dictionaries al eerder gemaakt?
-        idf = self.__load_file("idf.bin")
-        tweetDicts = self.__load_file("tweetdicts.bin")
-        
-        if idf and "%FILENAME%:" + tweetFile in idf and tweetDicts:
-            print("Successfully loaded the earlier generated binary files for ", tweetFile, "!", sep = "")
-            return tweetDicts, idf
-                print("Done! Dumping dictionaries to disk...")
-                
-        with open('idf.bin', 'wb') as f:
-            idf["%FILENAME%:" + tweetFile] = 1
-            msgpack.dump(idf, f)
-        print("* idf.bin is ready...")
-        
-        with open('tweetdicts.bin', 'wb') as f:
-            msgpack.dump(tweetDicts, f)
-        print("* tweetdicts.bin is ready...")
-        
-        # IDF DUMP
-        for word in tokens:
-            idf[word] += 1
-
-        # bereken idf-score voor ieder woord
-        n = len(idf)
-        for word in idf:
-            idf[word] = math.log10(n / idf[word])
-        """
+        pass
 
 # DEMO
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("./EventDetective.py tweetFile")
-        sys.exit()
-    start = time.time()
-    detective = EventDetective(sys.argv[1])
-    end = time.time() - start
-    print("Took", end, "seconds.")
+    detective = EventDetective()
