1fffb7464bb0b573e7eb6254c0560b4ce99d900a
daviddekleer <ddekleer@kpnplanet.nl>
2015-05-02 12:09:02 +0200

    CreateClassifiers toegevoegd (doet wat EventDetective deed en slaat de getrainde classifiers op). Iconen gemaakt voor het weergeven van de verschillende eventtypes op de kaart.

--- EventDetective.py
+++ EventDetective.py
@@ -21,19 +21,13 @@ from modules import tabulate
 class EventDetective:
 
     def __init__(self):
-        self.dataSets = os.listdir('data/')
-        self.idf = Counter()
-        self.annotation = {}
-        self.candidates = {}
-        self.__loadDataSet()
-        self.calculateIDF()
-        self.createFeatureTypes()
-        
-        self.classifyNLTK()
-        
-        #self.categorizeNLTK()
-        self.generateMarkers()
-    
+        #self.dataSets = os.listdir('data/')
+        #self.annotation = {}
+        #self.candidates = {}
+        #self.__loadDataSet()
+
+        #self.generateMarkers()
+    """
     def __loadDataSet(self):
         for i, dataset in enumerate(self.dataSets):
             print("{}: {}".format(i, dataset))
@@ -45,158 +39,6 @@ class EventDetective:
         with open("data/" + self.dataSets[choice] + "/sanitizedEventCandidates_cleaned.json") as jsonFile:
             self.candidates = json.load(jsonFile)
 
-
-    def calculateIDF(self):
-        n = 0        
-        for geohash in self.candidates:
-            for timestamp in self.candidates[geohash]:
-                # een tweet is een document (niet een cluster)
-                for tweet in self.candidates[geohash][timestamp]:
-                    self.idf.update(set(tweet['tokens']))
-                    n += 1
-        for word in self.idf:
-            self.idf[word] = log(n/self.idf[word])   
-    
-    
-    def selectDataset(self):
-        dataset = []
-        for h in self.candidates:
-            for t in self.candidates[h]:
-                dataset.append( (self.candidates[h][t],self.isEvent(h,t), self.eventType(h,t) ) )
-        
-        random.shuffle(dataset)
-        trainSplit = int(0.8 * len(dataset))
-        self.trainData = dataset[:trainSplit]
-        self.testData = dataset[trainSplit:]
-
-
-    def classifyNLTK(self):
-        print("Classify events")
-        accuracy = 0
-        baselineAvg = 0
-        table = [] 
-        
-        for i in range(5):
-            print("Iteration {}".format(i))
-            self.selectDataset()
-            testCat = []
-            trainCat = []
-            testBi = []
-            trainBi = []
-            
-            #first train category classifier
-            for candidate, event, label in self.testData:
-                featuresCat = self.wordFeatureSelector(candidate)
-                testCat.append((featuresCat, label))         
-            
-            for candidate, event, label in self.trainData:
-                featuresCat = self.wordFeatureSelector(candidate)
-                trainCat.append((featuresCat, label))
-
-            # MultinomialNB lijkt hier net zo goed als de nltk naive bayes classifier, maar is wel wat sneller
-            self.classifierCat = SklearnClassifier(MultinomialNB()).train(trainCat)
-            
-            '''ConfusionMatrix
-            ref = []
-            tagged =[]
-            for f, e in testCat:
-                ref.append(self.classifierCat.classify(f))
-                tagged.append(e)
-            
-            cm = nltk.ConfusionMatrix(ref, tagged)
-            print(cm)
-            '''
-            #second step train the event/no event classifier
-            for candidate, event, label in self.testData:
-                featuresBi = self.featureSelector(candidate)   
-                featureKeys = featuresBi.keys()
-                testBi.append((featuresBi, event)) 
-        
-            
-            for candidate, event, label in self.trainData:
-                featuresBi = self.featureSelector(candidate)
-                featureKeys = featuresBi.keys()
-                trainBi.append((featuresBi, event))
-
-            self.classifierBi = nltk.NaiveBayesClassifier.train(trainBi) #SklearnClassifier(LinearSVC()).train(trainBi)
-
-            refsets = defaultdict(set)
-            testsets = defaultdict(set)
-            baseline = defaultdict(set)
-
-            for n, (feats, label) in enumerate(testBi):
-                refsets[label].add(n)
-                observed = self.classifierBi.classify(feats)
-                testsets[observed].add(n)
-                baseline['event'].add(n)
-
-            a = nltk.classify.accuracy(self.classifierBi,testBi)
-            accuracy += a
-            
-            baseL = nltk.metrics.precision(refsets['event'], baseline['event']) #precision event
-            baselineAvg += baseL
-            
-            pEvent = nltk.metrics.precision(refsets['event'], testsets['event']) #precision event
-            rEvent = nltk.metrics.recall(refsets['event'], testsets['event']) #recall event
-            fEvent = nltk.metrics.f_measure(refsets['event'], testsets['event']) #f score
-
-            pNoEvent = nltk.metrics.precision(refsets['noEvent'], testsets['noEvent']) #precision no event
-            rNoEvent = nltk.metrics.recall(refsets['noEvent'], testsets['noEvent']) #recall no event
-            fNoEvent = nltk.metrics.f_measure(refsets['noEvent'], testsets['noEvent']) #f score no event
-
-            table.append([i,round(baseL, 2), round(a, 2), round(pEvent,2), round(rEvent,2),round(fEvent,2),round(pNoEvent,2), round(rNoEvent,2), round(fNoEvent,2)])
-        print()
-        print("Classification using features: {} | training set size: {} & test set size: {} ".format(", ".join(featureKeys),len(trainBi), len(testBi)))  
-        print()
-        print (tabulate.tabulate(table, headers=['#', 'Baseline', 'Accuracy', 'Pre. Event','Rec. Event','F. Event','Pre. No-event','Rec. No-event','F. No_Event']))
-        print("Avg accuracy = {}".format(round(accuracy / (i + 1) , 2)))
-        print("Avg baseline accuracy (everything is an event)= {}".format(round(baselineAvg / (i + 1) , 2)))
-        print()
-
-
-    def createFeatureTypes(self):
-        '''get all possible word features'''
-        featureTypes = Counter()
-        for g in self.candidates:
-            for t in self.candidates[g]:
-                candidate = self.candidates[g][t]
-                #if self.classifier.classify(self.featureSelector(candidate)) == 'event':
-                for row in candidate:
-                    featureTypes.update(row['tokens'])
-        
-        
-        for f in featureTypes:
-            featureTypes[f] = featureTypes[f] * self.idf[f]
-
-        self.features = [word for word, n in featureTypes.most_common(800)]
-
-    
-    def wordFeatureSelector(self, candidate):
-        candidateFeatures = {}
-        for row in candidate:
-            for feature in self.features:
-                if feature in row['tokens']:
-                    candidateFeatures[feature] = True
-                else:
-                    if feature not in candidateFeatures:
-                        candidateFeatures[feature] = False 
-        return candidateFeatures     
-
-
-    def featureSelector(self, cluster):
-        featuresDict = {}
-        #featuresDict['overlap'] = features.wordOverlap(cluster)
-        #featuresDict['overlapSimple'] = features.wordOverlapSimple(cluster)
-        featuresDict['overlapUser'] = features.wordOverlapUser(cluster)
-        #featuresDict['nUsers'] = features.uniqueUsers(cluster)
-        #featuresDict['nTweets'] = features.nTweets(cluster)
-        #featuresDict['atRatio'] = features.atRatio(cluster) 
-        featuresDict['overlapHashtags'] = features.overlapHashtags(cluster)
-        #featuresDict['averageTfIdf'] = features.averageTfIdf(cluster, self.idf)
-        featuresDict['category'] = self.classifierCat.classify(self.wordFeatureSelector(cluster))
-        featuresDict['location'] = features.location(cluster) # locatie maakt niet heel veel uit
-        return featuresDict
-    
     def eventType(self,geohash,timestamp):
         # return values {strings gebruiken?}
         eventTypes = {0:"geen event", 1:"sport", 2:"entertainment", 3:"bijeenkomst", 4:"incident", 5:"anders"}
@@ -241,19 +83,17 @@ class EventDetective:
                 
                 avgLon /= i
                 avgLat /= i
-                features = self.featureSelector(self.candidates[geohash][timestamp])
-                featureString = ''
-                for key in features:
-                    featureString += " {} - {} |".format(key, features[key])
-                writableCluster += featureString
+                #features = self.featureSelector(self.candidates[geohash][timestamp])
+                #featureString = ''
+                #for key in features:
+                #    featureString += " {} - {} |".format(key, features[key])
+                #writableCluster += featureString
                 # JS file maken voor Google maps
-                result = self.classifierBi.classify(features)
-                if result == 'event':
-                    js.write("['{}', {}, {}, '{}', '{}'],".format(writableCluster, avgLat,avgLon, result,self.isEvent(geohash, timestamp)))
+                #result = self.classifierBi.classify(features)
+                #if result == 'event':
+                #    js.write("['{}', {}, {}, '{}', '{}'],".format(writableCluster, avgLat,avgLon, result,self.isEvent(geohash, timestamp)))
         js.write('];')
-        js.close()
-
-
+        js.close()"""
 
 # DEMO
 if __name__ == "__main__":
