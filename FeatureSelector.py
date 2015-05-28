ea073051b4b4708945175ceffa7c23f18e29ab48
daviddekleer <ddekleer@kpnplanet.nl>
2015-05-28 10:24:10 +0200

    Nog wat meer opschoning...

--- FeatureSelector.py
+++ FeatureSelector.py
@@ -86,7 +86,7 @@ class FeatureSelector:
         return returnFeatures
 
     def _wordOverlapUser(self, candidate):
-        '''Calculate the overlap of features among users, high score when high idf types are in each tweet'''
+        '''Calculate the overlap of features among users, high score when df value for a type is high'''
         userTypes = defaultdict(list)
         types = Counter()
 
@@ -97,15 +97,14 @@ class FeatureSelector:
             types.update(set(userTypes[user]))
         score = 0
         for t in types:
-            if types[t] > 1: #ignore if only in one tweet
+            if types[t] > 1: # ignore if type occurs only in one tweet
                 if t[0] == '#':
                     score += (types[t] * 2)
                 else:
                     score += types[t]
 
         if score > 1:
-            s = log(float(score) * float(len(userTypes.keys()) )) 
-            #return round((score * 2) / len(candidate))
+            s = log(score * len(userTypes.keys()))
             return round((s / len(candidate) )* 2 ) /2 
         else:
             return 0.0
@@ -125,13 +124,13 @@ class FeatureSelector:
         return round((score / len(candidate)) * 2) / 2
 
     def _wordOverlap(self, candidate):
-        types = Counter() #counter with n of tweets this term occurs
+        types = Counter() # counter with n of tweets this term occurs
         idf = defaultdict(float)
         n = len(candidate)
         for row in candidate:
             types.update(set(row['tokens']))
-        #calculate cluster IDF, hoe waardevol is woord in cluster
-        #IDF(t) = log_e(Total number of documents / Number of documents with term t in it).   
+        # calculate cluster IDF, hoe waardevol is woord in cluster
+        # IDF(t) = log(Total number of documents / Number of documents with term t in it).   
         for t in types:
             idf[t] += log(n/types[t]) 
             if t[0] == '#':
@@ -139,7 +138,7 @@ class FeatureSelector:
         
         score = 0
         for t in types:
-            if types[t] > 1: #ignore if only in one tweet
+            if types[t] > 1: # ignore if type occurs only in one tweet
                 score += types[t] * idf[t]
         
         if score == 0:
@@ -147,39 +146,9 @@ class FeatureSelector:
         else:
             return round((score / n) * 2) / 2 # round to 0.5
 
-    def _wordOverlapDisplay(self, candidate):
-        types = Counter() #counter with n of tweets this term occurs
-        tokens = Counter()
-        idf = defaultdict(float)
-        n = len(candidate)
-        for row in candidate:    
-            types.update(set(row['tokens']))
-            tokens.update(row['tokens'])  
-        
-        #calculate cluster IDF, hoe waardevol is woord in cluster
-        #IDF(t) = log_e(Total number of documents / Number of documents with term t in it).
-        
-        for t in types:
-            idf[t] += log(n/types[t]) 
-            if t[0] == '#':
-                idf[t] *= 3
-        maxscore = 0
-        score = 0
-        for t in types:
-            maxscore += n * tokens[t]
-            if types[t] > 1: #ignore if only in one tweet
-                score += types[t] * idf[t]
-        if score == 0 or n == 0:
-            return 0
-        else:
-            
-            value = round((score / n) * 2) / 2 #round to 0.5
-            common = [w + "(" + str(tokens[w]) + str(c * idf[t]) + ")" for w,c in types.most_common(3)]
-            return "Score: {:.2f} , Overlapping words: {} ".format( value, ' '.join(common ))
-
-    # Bepaal de gemiddelde locatie (lengte- en breedtegraad, omgezet
-    # naar geohash) van alle gebruikers in een cluster
     def _location(self, candidate):
+        """Bepaal de gemiddelde locatie (lengte- en breedtegraad, omgezet
+        naar geohash) van alle gebruikers in een cluster."""
         avgLon = 0
         avgLat = 0
         
